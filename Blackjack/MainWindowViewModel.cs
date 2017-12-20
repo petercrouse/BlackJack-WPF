dac5e1816126e4a874cb0532e46bab68521ed964
Peter Crouse (Information Technology) <PeterCrouse@capitecbank.co.za>
2017-12-20 08:24:19 +0200

    Add project files.

---
+++ Blackjack/MainWindowViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    public class MainWindowViewModel : Notifier
+    {
+        #region Properties
+        private HandViewModel _playerHand;
+        public HandViewModel PlayerHand { get; private set; }
+        private HandViewModel _dealerHand;
+        public HandViewModel DealerHand { get; private set; }
+
+        private int _cardsInDeck;
+        public int CardsInDeck
+        {
+            get { return _cardsInDeck; }
+            set
+            {
+                _cardsInDeck = value;
+                OnPropertyChanged("CardsInDeck");
+            }
+        }
+
+        private int _playerScore;
+        public int PlayerScore
+        {
+            get { return _playerScore; }
+            set
+            {
+                _playerScore = value;
+                OnPropertyChanged("PlayerScore");
+            }
+        }
+        private int _dealerScore;
+        public int DealerScore
+        {
+            get { return _dealerScore; }
+            set
+            {
+                _dealerScore = value;
+                OnPropertyChanged("DealerScore");
+            }
+        }
+        private Deck _myDeck;
+        public Deck MyDeck
+        {
+            get { return _myDeck; }
+            set
+            {
+                _myDeck = value;
+                OnPropertyChanged("MyDeck");
+            }
+        }
+        #endregion  
+        public MainWindowViewModel()
+        {
+            PlayerHand = new HandViewModel();
+            DealerHand = new HandViewModel();
+            PlayerScore = 0;
+            DealerScore = 0;
+            MyDeck = new Deck();
+            CardsInDeck = MyDeck.CardsLeft();
+            NewGame();
+        }
+        
+        private void DealHands()
+        {
+            PlayerHand.AddCard(MyDeck.pick());
+            PlayerHand.AddCard(MyDeck.pick());
+            DealerHand.AddCard(MyDeck.pick());
+            DealerHand.AddCard(MyDeck.pick());
+        }
+        public void NewGame()
+        {
+            DealHands();
+            CardsInDeck = MyDeck.CardsLeft();
+        }
+    }
+}
