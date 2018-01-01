3061bffcb377fae3a6bf25852114a2d379120407
Peter Crouse <pacrouse@gmail.com>
2018-01-01 17:24:15 +0200

    Fixesd the Title

--- Blackjack.client/ViewModels/MainWindowViewModel.cs
+++ Blackjack.client/ViewModels/MainWindowViewModel.cs
@@ -7,7 +7,7 @@ namespace Blackjack.client.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "Blackjack21";
         public string Title
         {
             get { return _title; }
