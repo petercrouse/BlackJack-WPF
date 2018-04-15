c140da8eef5e9657c663bcde85fe79e338440bda
Peter Crouse <pacrouse@gmail.com>
2018-04-15 22:06:31 +0200

    changed the GameUser and Scoreboard entites to include Id and restrictions

--- Blackjack.models/Entities/GameUser.cs
+++ Blackjack.models/Entities/GameUser.cs
@@ -1,8 +1,14 @@
-﻿namespace Blackjack.models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blackjack.models.Entities
 
 {
     public class GameUser
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(10)]
         public string Alias { get; set; }
         public string Email { get; set; }
     }
--- Blackjack.models/Entities/Scoreboard.cs
+++ Blackjack.models/Entities/Scoreboard.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Blackjack.models.Entities
 {
     public class Scoreboard
     {
-        public string Alias { get; set; }
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public GameUser GameUser { get; set; }
+        [Required]
         public int Score { get; set; }
+        [Required]
         public string Game { get; set; }
         public DateTimeOffset Date { get; set; }
     }
