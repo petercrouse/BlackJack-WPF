dc044b0eb4b33741cb2a18c391792bcf6aa76eaf
Peter Crouse <pacrouse@gmail.com>
2018-04-18 23:20:53 +0200

    Add DBContexts

---
+++ BlackJack.persistence/GameContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Blackjack.models.Entities;
+
+namespace BlackJack.persistence
+{
+    public class GameContext : DbContext, IDbContext
+    {
+        public GameContext() : base("name=GameDB")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<GameContext>());
+            Database.SetInitializer<GameContext>(null);
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public new DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : GameEntity
+        {
+            return base.Entry(entity);
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : GameEntity
+        {
+            return base.Set<TEntity>();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
