d3cc9d6a017b0823d407d86bbdcebd26bc6d3d1b
Peter Crouse <pacrouse@gmail.com>
2018-04-18 22:23:17 +0200

    added IRepository

---
+++ BlackJack.persistence/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Blackjack.models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.persistence
+{
+    public interface IRepository<TEntity> where TEntity : GameEntity
+    {
+        void Add(TEntity);
+        void Edit(Guid referenceId);
+        TEntity Get(Guid referenceId);
+
+    }
+}
